26a4d71076615875381e276ad5924175fa5e9fe7
Jason Gross <jgross@mit.edu>
2020-05-20 15:05:42 -0400

    Cleaner version of set -x stuff
    h/t @SkySkimmer at
    https://github.com/mit-plv/engine-bench/commit/81d9328d252934054c3a149961cf9c74664be795#r39309148

--- etc/machine-extended.sh
+++ etc/machine-extended.sh
@@ -1,8 +1,18 @@
 #!/bin/sh
 
-echo '+ lscpu'
-lscpu || true
-echo '+ uname -a'
-uname -a || true
-echo '+ lsb_release -a'
-lsb_release -a || true
+# save a way to get the stderr
+exec 3>&2
+
+# output set -x stuff to stdout
+exec 2>&1
+
+# put all the commands in a subshell so we don't print true at the end
+(
+set -x
+
+# redirect errors to stderr
+lscpu 2>&3
+uname -a 2>&3
+lsb_release -a 2>&3
+)
+true # never fail
