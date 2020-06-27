c58be4811d624cc9941a3b04f3c4ed4680477c21
Jason Gross <jgross@mit.edu>
2020-06-27 19:46:48 -0400

    Add Ltac2 fast_rewrite

--- coq/PerformanceExperiments/rewrite_repeated_app_fast_rewrite_ltac2.v
+++ coq/PerformanceExperiments/rewrite_repeated_app_fast_rewrite_ltac2.v
@@ -1,6 +1,6 @@
 Require Import PerformanceExperiments.Harness.
 Require Import PerformanceExperiments.HarnessTimeAbstract.
-Require Export PerformanceExperiments.rewrite_repeated_app_common.
+Require Export PerformanceExperiments.rewrite_repeated_app_common_ltac2.
 
 Definition args_of_size (s : size) : list nat
   := match s with
@@ -8,7 +8,8 @@ Definition args_of_size (s : size) : list nat
      | SuperFast => List.map (fun x => x * 50) (seq 1 8)
      | Fast => (seq 1 100)
                  ++ List.map (fun x => x * 10) (seq 1 40)
-                 ++ List.map (fun x => x * 100) (seq 1 10)
+                 ++ List.map (fun x => x * 100) (seq 1 20)
+                 ++ List.map (fun x => x * 500) (seq 1 6)
      | Medium => []
      | Slow => []
      | VerySlow => []
@@ -16,11 +17,11 @@ Definition args_of_size (s : size) : list nat
 
 Ltac time_solve_goal0 n :=
   time_abstract_regression_cubic
-    (time "fast_rewrite-regression-cubic" fast_rewrite).
+    (time "fast_rewrite-regression-cubic" fast_rewrite_ltac2).
 
 Ltac run0 sz := Harness.runtests args_of_size default_describe_goal mkgoal redgoal time_solve_goal0 sz.
 
 (*
 Goal True.
-  run0 Fast.
+  Time run0 Fast.
  *)
