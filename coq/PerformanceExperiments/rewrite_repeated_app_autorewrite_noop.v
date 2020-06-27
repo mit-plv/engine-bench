ad25925b3ec50a126134ecadc479390f5e4665f8
Jason Gross <jgross@mit.edu>
2020-06-27 16:42:04 -0400

    Update logs

--- coq/PerformanceExperiments/rewrite_repeated_app_autorewrite_noop.v
+++ coq/PerformanceExperiments/rewrite_repeated_app_autorewrite_noop.v
@@ -5,7 +5,7 @@ Definition args_of_size (s : size) : list nat
   := match s with
      | Sanity => seq 1 3
      | SuperFast => List.map (fun x => x * 50) (seq 1 8)
-     | Fast => (seq 1 1000)
+     | Fast => (List.map (fun x => x * 4) (seq 1 250))
                  ++ (List.map (fun x => x * 100) (seq 1 50))
                  ++ (List.map (fun x => x * 500) (seq 1 20))
      | Medium => []
@@ -24,8 +24,8 @@ Ltac do_ssr_rewrite_ques := try rewrite ?fg.
 
 Ltac time_solve_goal0 n :=
   time "autorewrite-noop-regression-quadratic" do_autorewrite;
-  time "try-rewrite!-noop-regression-linear" do_rewrite_bang;
-  time "try-rewrite-noop-regression-linear" do_rewrite_once;
+  time "try-rewrite!-noop-regression-quadratic" do_rewrite_bang;
+  time "try-rewrite-noop-regression-quadratic" do_rewrite_once;
   time "try-rewrite?-noop-regression-quadratic" do_rewrite_ques;
   time "try-ssr-rewrite!-noop-regression-linear" do_ssr_rewrite_bang;
   time "try-ssr-rewrite-noop-regression-linear" do_ssr_rewrite_once;
